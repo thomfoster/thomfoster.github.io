049e1ebccb7bad5cbcd3205e03104d8d0997d1f9
thomfoster <36595849+thomfoster@users.noreply.github.com>
2023-01-17 21:40:12 +0000

    dynamically rendering timeline content from file

---
+++ assets/timeline.js
@@ -0,0 +1,23 @@
+const timeline = [
+    {
+        "id": "genei",
+        "label": "2019-Now",
+        "title": "Genei.io",
+        "subtitle": "Founder and Chief Science Officer",
+        "body": "Using large language models to speed up research. Think ChatGPT but scoped to documents you upload. We went through YC combinator in 2021 and recieved funding from some great people. If you're passionate about applying all the recent advances of generative AI, we're hiring"
+    },
+    {
+        "id": "halo",
+        "label": "2020",
+        "title": "Halo",
+        "subtitle": "Founder and Chief Technology Officer",
+        "body": "Whirlwind ride trying to help out during the pandemic. Started doing PCR tests out of a shipping container with a friend from uni. Scaled to 1,000 tests a day. I've written more about my experiences with Halo here."
+    },
+    {
+        "id": "oxford",
+        "label": "2016-2020",
+        "title": "University of Oxford",
+        "subtitle": "Master's in Computer Science",
+        "body": "2nd in cohort. Microsoft prize for best undergrad dissertation."
+    },
+]
