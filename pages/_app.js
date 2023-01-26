20464950e69003084a7cf59a85d05f51ca9c7d7f
thomfoster <36595849+thomfoster@users.noreply.github.com>
2023-01-26 10:10:44 +0100

    add @vercel/analytics tracking

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,13 @@ import resetStyles from "../styles/reset.css";
 import layoutStyles from "../styles/main_layout.css";
 import timelineStyles from "../styles/timeline.css";
 import styles from "../styles/styles.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
