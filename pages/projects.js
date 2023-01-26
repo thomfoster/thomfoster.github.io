c7e5380e6602a604aa075e11d558b1ca9eda25d8
thomfoster <36595849+thomfoster@users.noreply.github.com>
2023-01-26 11:09:48 +0100

    add new pages and update layout css to work with that

---
+++ pages/projects.js
@@ -0,0 +1,15 @@
+import Layout from "../components/layout";
+
+export default function Research() {
+    return (
+        <Layout>
+            <section>
+                Here are some projects I've been up to!
+                <ul>
+                    <li>minRLHF: A tutorial on RLHF</li>
+                    <li>wikigusser: A fun game for testing your general knowledge</li>
+                </ul>
+            </section>
+        </Layout>
+    )
+}
---
+++ pages/research.js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout";
+
+export default function Research() {
+    return (
+        <Layout>
+            This is some text about my research.
+        </Layout>
+    )
+}
